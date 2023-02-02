dd123d5d4947622b67b8ee2148f0d22f62d4744e
Simon <sblaue@gmx.de>
2023-02-02 12:12:50 +0100

    better readme

--- src/model.py
+++ src/model.py
@@ -1,2 +1,17 @@
 import numpy as np
 
+
+class WalkerConf():
+    def __init__(self, radius, noise_strength) -> None:
+        self.radius = radius
+        self.noise_strength = noise_strength
+        
+
+def init_walkers(n):
+    # Array with posx, posy, orientation 
+    walkers = np.random.rand(n, 3)
+    return walkers
+
+
+def step(walkers):
+    return 
