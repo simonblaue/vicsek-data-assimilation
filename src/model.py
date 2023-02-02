dc1945e1f4db60da2af6f68b0d500998b8f2e022
Simon <sblaue@gmx.de>
2023-02-02 13:28:04 +0100

    distances done

--- src/model.py
+++ src/model.py
@@ -1,6 +1,9 @@
 import numpy as np
 from config import SimulationConfig
 
+from itertools import permutations
+
+
 
 class Simulation:
 
@@ -11,8 +14,21 @@ class Simulation:
         self.walkers = np.random.rand(self.config.n_particles, 3)
         self.time = 0
         
-    # TODO
+
+    def distances(self):
+        distances = np.zeros((self.config.n_particles, self.config.n_particles)) 
+        walker_pos = self.walkers[:,0:2]
+
+        for i,walker in enumerate(walker_pos):
+            distances[i,:] = np.linalg.norm(walker_pos - walker, axis=1)
+
+        return distances
+        
+    
     def step(self):
+        # Calculate all distances to every other walker
+
+        # In 
         
         self.time += self.config.timestepsize
         return 
