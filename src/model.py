b04fbe11f2a7e3184ffb0b8b9b3bfc593ddb3efe
Simon <sblaue@gmx.de>
2023-02-02 12:34:15 +0100

    grobe struktur

--- src/model.py
+++ src/model.py
@@ -1,17 +1,20 @@
 import numpy as np
 
 
-class WalkerConf():
-    def __init__(self, radius, noise_strength) -> None:
-        self.radius = radius
-        self.noise_strength = noise_strength
-        
 
-def init_walkers(n):
-    # Array with posx, posy, orientation 
-    walkers = np.random.rand(n, 3)
-    return walkers
+class Simulation:
+
+    def __init__(self):
+        # Init Config
+        self.config = config.SimulationConfig()
+        # Array with posx, posy, orientation as rad for n walkers
+        self.walkers = np.random.rand(self.config.n_particles, 3)
 
 
-def step(walkers):
-    return 
+    # TODO
+    def step(self):
+        return 
+    
+    # TODO
+    def plot(self):
+        return
