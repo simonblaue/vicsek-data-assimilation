960d363b9f0d9b2a790456b11295c64b1a8c399f
Simon <sblaue@gmx.de>
2023-02-02 12:47:18 +0100

    added todos

--- src/model.py
+++ src/model.py
@@ -1,20 +1,23 @@
 import numpy as np
-
+from config import SimulationConfig
 
 
 class Simulation:
 
     def __init__(self):
         # Init Config
-        self.config = config.SimulationConfig()
+        self.config = SimulationConfig()
         # Array with posx, posy, orientation as rad for n walkers
         self.walkers = np.random.rand(self.config.n_particles, 3)
-
-
+        self.time = 0
+        
     # TODO
     def step(self):
+        
+        self.time += self.config.timestepsize
         return 
     
     # TODO
     def plot(self):
         return
+    
