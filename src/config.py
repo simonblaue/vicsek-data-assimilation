471cab5ebd7c5b8a11f2a8014a7c22761d16cbe9
Simon <sblaue@gmx.de>
2023-02-03 11:43:33 +0100

    ordered movement geht auch

--- src/config.py
+++ src/config.py
@@ -17,13 +17,36 @@ class SimulationConfig:
     alignment_radius: float = 1
 
     # field
-    x_axis = 7
-    y_axis = 7
+    x_axis = 25
+    y_axis = 25
 
     # simulation
     velocity: float = 0.03
-    noisestrength: float = 2.0
+    noisestrength: float = 0.1
 
     endtime: float = 100
     timestepsize: float = 1.0
 
+
+
+###
+
+# Das sind base parameter für random movement:
+
+# @dataclass
+# class SimulationConfig:
+#     # particles
+#     n_particles: int = 300
+#     # repulsion_radius: float = 0.5
+#     alignment_radius: float = 1
+
+#     # field
+#     x_axis = 7
+#     y_axis = 7
+
+#     # simulation
+#     velocity: float = 0.03
+#     noisestrength: float = 2.0
+
+#     endtime: float = 100
+#     timestepsize: float = 1.0
