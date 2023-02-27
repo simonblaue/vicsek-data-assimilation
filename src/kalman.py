117bf1068306915168f174e4d4dd020a8c51c625
Thenrik404 <training.deepl@gmail.com>
2023-02-27 15:18:04 +0100

    added kalmanconfig

--- src/kalman.py
+++ src/kalman.py
@@ -1,27 +1,30 @@
 import numpy as np
 from vicsek import RandomSimulationConfig, ViszecSimulation
-
-model_forecast = ViszecSimulation._step
-
+from dataclasses import dataclass
 
 class EnsembleKalman():
     def __init__(self, config):
 
             self.config = config
 
-            self.state = 
+            self.state = np.random.rand(self.config.n_particles, 3)
             # ensemble size
-            self.N = 5
+            self.N = self.config.n_ensembles
             # noise parameter
-            self.r = 0.01
+            self.r = self.config.r
             
-    def update(self, measurement):
+            model_forecast = ViszecSimulation._step
+            
+    def update(self, measurement: np.ndarray) -> np.ndarray:
         #generating ensamples
-        forecast_ensemble = [model_forecast(self.state) for _ in range(self.N)]
+        forecast_ensemble = [
+            self.model_forecast(self.state) for _ in range(self.N)
+        ]
         measurement_ensemble = np.array([measurement for _ in range(self.N)])
         noise = np.random.normal(size=(300, 3), scale=self.r)
         virtual_observations = measurement_ensemble+noise
         
+        # forecast matrix
         mean_forecast = np.mean(forecast_ensemble, axis = 0)
         errors = [f-mean_forecast for f in forecast_ensemble]
         pf = 1/(self.N-1) * np.sum(
@@ -32,7 +35,7 @@ class EnsembleKalman():
         R = np.diag(np.ones(RandomSimulationConfig.n_particles))*self.r
         
         K = pf*np.linalg.inv(pf+R)
-        
+        # update
         ensemble_update = [
             x + K @ (z-x) for x, z in zip(forecast_ensemble, virtual_observations)
         ]
@@ -40,3 +43,13 @@ class EnsembleKalman():
         self.state = np.mean(ensemble_update, axis = 0)
         
         return self.state
+
+
+@dataclass
+class EnsembleKalmanConfig:
+    
+    exec_ref = EnsembleKalman
+    
+    n_ensembles: int = 5
+    
+    r: float = 0.01
