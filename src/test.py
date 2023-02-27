e7b4ff90891327854b36921719c839f1203b185a
Simon <sblaue@gmx.de>
2023-02-27 14:02:22 +0100

    distance bound

--- src/test.py
+++ src/test.py
@@ -5,4 +5,4 @@ import vicsek
 if __name__ =="__main__":
     anim = animation.VicsekAnimationConfig.exec_ref(animation.VicsekAnimationConfig,vicsek.OrderedSimulationConfig)
     anim(save_name="test1")
-
+# In gitignore
