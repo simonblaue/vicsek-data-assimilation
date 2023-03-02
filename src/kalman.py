0f313d71726c8c0396de45c9590b5b1167bb52a6
Simon <sblaue@gmx.de>
2023-03-02 15:03:28 +0100

    preped measurements

--- src/kalman.py
+++ src/kalman.py
@@ -14,13 +14,11 @@ class EnsembleKalman():
         
         """
         self.config = config
-        # self.agents = init_agents.copy()
-        # self.agents = init_agents.copy()
         self.agents = np.random.rand(self.config["n_particles"], 5)
         self.agents[:,0] *= self.config["x_axis"]
         self.agents[:,1] *= self.config["y_axis"]
-        self.agents[:,2] = (np.random.rand(self.config["n_particles"])) * config['velocity']
-        self.agents[:,3] = (np.random.rand(self.config["n_particles"])) * config['velocity']
+        self.agents[:,2] = config['velocity'] #* (np.random.rand(self.config["n_particles"])) * 
+        self.agents[:,3] = config['velocity'] #* (np.random.rand(self.config["n_particles"]))
 
         # print(self.agents.shape)
         self.model_forecast = forecast_func
